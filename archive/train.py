04ae047655b1fe1831bcb243e650d10e882a92ac
saoyan <yanyiqinwpu@gmail.com>
2018-12-28 11:46:05 -0800

    major re-write

--- archive/train.py
+++ archive/train.py
@@ -13,35 +13,54 @@ from torchvision import transforms, utils, datasets
 from PIL import Image
 from tensorboardX import SummaryWriter
 from networks import Generator, Discriminator
+from data_2017 import preprocess_data_gan, ISIC_GAN
+from transforms import *
+
+###
+# train for stage 1-7
+# device: 4 NVIDIA P100 Pascal GPUs
+# training time:
+# stage 1: 50 epoch
+# stage 2-4: 50 epoch transition + 50 epoch stability
+# stage 5-7: 50 epoch transition + 100 epoch stability
+###
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
 
 torch.backends.cudnn.benchmark = True
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device_ids = [0]
+device_ids = [0,1,2,3]
 
 parser = argparse.ArgumentParser(description="PGAN-Skin-Lesion")
 
 parser.add_argument("--preprocess", action='store_true')
 
 parser.add_argument("--nc", type=int, default=3, help="number of channels of the generated image")
-parser.add_argument("--nz", type=int, default=256, help="dimension of the input noise")
-parser.add_argument("--size", type=int, default=32, help="the final size of the generated image")
+parser.add_argument("--nz", type=int, default=512, help="dimension of the input noise")
+parser.add_argument("--size", type=int, default=256, help="the final size of the generated image")
 
-parser.add_argument("--batch_size", type=int, default=64)
+parser.add_argument("--batch_size", type=int, default=16)
 parser.add_argument("--unit_epoch", type=int, default=50)
-parser.add_argument("--num_aug", type=int, default=1, help="times of data augmentation (num_aug times through the dataset is one actual epoch)")
+parser.add_argument("--num_aug", type=int, default=10, help="times of data augmentation (num_aug times through the dataset is one actual epoch)")
 parser.add_argument("--lr", type=float, default=0.001, help="initial learning rate")
 parser.add_argument("--outf", type=str, default="logs", help='path of log files')
 
 opt = parser.parse_args()
 
+'''
+switch between ISIC 2017 and 2018
+modify the following contents:
+1. import
+2. root_dir of preprocess_data
+3. num_aug
+'''
+
 #----------------------------------------------------------------------------
 # Trainer
 
-def __worker_init_fn__():
+def _worker_init_fn_():
     torch_seed = torch.initial_seed()
     np_seed = torch_seed // 2**32-1
     random.seed(torch_seed)
@@ -50,7 +69,7 @@ def __worker_init_fn__():
 class trainer:
     def __init__(self):
         print("\ninitializing trainer ...\n")
-        self.intial_size = 4
+        self.initial_size = 4
         self.writer = SummaryWriter(opt.outf)
         self.init_trainer()
         print("\ndone\n")
@@ -69,43 +88,61 @@ class trainer:
         self.opt_G = optim.Adam(self.G.parameters(), lr=opt.lr, betas=(0,0.99), eps=1e-8, weight_decay=0.)
         self.opt_D = optim.Adam(self.D.parameters(), lr=opt.lr, betas=(0,0.99), eps=1e-8, weight_decay=0.)
         # data loader
-        self.transform = transforms.ToTensor()
-        self.dataset = datasets.CIFAR10(root='CIFAR10_data', train=True, download=True, transform=self.transform)
+        self.transform = transforms.Compose([
+            RatioCenterCrop(1.),
+            transforms.Resize((300,300), Image.ANTIALIAS),
+            transforms.RandomCrop((opt.size,opt.size)),
+            RandomRotate(),
+            transforms.RandomVerticalFlip(),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor()
+        ])
+        self.dataset = ISIC_GAN('train_gan.csv', shuffle=True, transform=self.transform)
         self.dataloader = torch.utils.data.DataLoader(self.dataset, batch_size=opt.batch_size,
-            shuffle=True, num_workers=8, worker_init_fn=__worker_init_fn__(), drop_last=True)
-        # tickers (used for fading in)
-        self.tickers = opt.unit_epoch * opt.num_aug * len(self.dataloader)
-    def update_trainer(self, stage, inter_ticker):
+            shuffle=True, num_workers=8, worker_init_fn=_worker_init_fn_(), drop_last=True)
+    def update_trainer(self, stage, inter_epoch):
         """
         update status of trainer
         :param stage: stage number; starting from 1
-        :param inter_ticker: ticker number within the current stage; starting from 0 within each stage
+        :param inter_epoch: epoch number within the current stage; starting from 0 within each stage
         :return current_alpha: value of alpha (parameter for fade in) after updating trainer
         """
+        flag_opt = False
+        print("\nupdating trainer ...\n")
         if stage == 1:
+            assert inter_epoch < opt.unit_epoch, 'Invalid epoch number!'
             current_alpha = 0
+            print("\nnothing to update about trainer ...\n")
         else:
-            flag_opt = False
-            delta = 1. / self.tickers
             total_stages = int(math.log2(opt.size/4)) + 1
             assert stage <= total_stages, 'Invalid stage number!'
-            if inter_ticker == 0:
+            if stage <= 4:
+                assert inter_epoch < opt.unit_epoch * 2, 'Invalid epoch number!'
+            else:
+                assert inter_epoch < opt.unit_epoch * 3, 'Invalid epoch number!'
+            delta = 1. / (opt.unit_epoch-1.)
+            if inter_epoch == 0:
+                print("\ngrow networks ...\n")
                 self.G.module.grow_network()
                 self.D.module.grow_network()
                 self.G_EMA.grow_network()
                 flag_opt = True
-            elif (inter_ticker > 0) and (inter_ticker < self.tickers):
+            elif (inter_epoch > 0) and (inter_epoch < opt.unit_epoch):
+                print("\nfade in ...\n")
                 self.G.module.model.fadein.update_alpha(delta)
                 self.D.module.model.fadein.update_alpha(delta)
                 self.G_EMA.model.fadein.update_alpha(delta)
                 flag_opt = False
-            elif inter_ticker == self.tickers:
+            elif inter_epoch == opt.unit_epoch:
+                print("\nflush networks ...\n")
                 self.G.module.flush_network()
                 self.D.module.flush_network()
                 self.G_EMA.flush_network()
                 flag_opt = True
             else:
-                flag_opt = False
+                # stablization
+                print("\nnothing to update ...\n")
+                flag_opt = False;
             # archive alpha
             try:
                 current_alpha = self.G.module.model.fadein.get_alpha()
@@ -113,33 +150,21 @@ class trainer:
                 current_alpha = 1
             # move to devie & update optimizer
             if flag_opt:
+                print("\nmove to device and update optimizer ...\n")
                 self.G.to(device)
                 self.D.to(device)
                 self.G_EMA.to('cpu')
-                # opt_G
-                opt_G_state_dict = self.opt_G.state_dict()
-                old_opt_G_state = opt_G_state_dict['state']
                 self.opt_G = optim.Adam(self.G.parameters(), lr=opt.lr, betas=(0,0.99), eps=1e-8, weight_decay=0.)
-                new_opt_G_param_id =  self.opt_G.state_dict()['param_groups'][0]['params']
-                opt_G_state = copy.deepcopy(old_opt_G_state)
-                for key in old_opt_G_state.keys():
-                    if key not in new_opt_G_param_id:
-                        del opt_G_state[key]
-                opt_G_state_dict['param_groups'] = self.opt_G.state_dict()['param_groups']
-                opt_G_state_dict['state'] = opt_G_state
-                self.opt_G.load_state_dict(opt_G_state_dict)
-                # opt_D
-                opt_D_state_dict = self.opt_D.state_dict()
-                old_opt_D_state = opt_D_state_dict['state']
                 self.opt_D = optim.Adam(self.D.parameters(), lr=opt.lr, betas=(0,0.99), eps=1e-8, weight_decay=0.)
-                new_opt_D_param_id =  self.opt_D.state_dict()['param_groups'][0]['params']
-                opt_D_state = copy.deepcopy(old_opt_D_state)
-                for key in old_opt_D_state.keys():
-                    if key not in new_opt_D_param_id:
-                        del opt_D_state[key]
-                opt_D_state_dict['param_groups'] = self.opt_D.state_dict()['param_groups']
-                opt_D_state_dict['state'] = opt_D_state
-                self.opt_D.load_state_dict(opt_D_state_dict)
+                # opt_G_state_dict = self.opt_G.state_dict()
+                # opt_D_state_dict = self.opt_D.state_dict()
+                # self.opt_G = optim.Adam(self.G.parameters(), lr=opt.lr, betas=(0,0.99), eps=1e-8, weight_decay=0.)
+                # self.opt_D = optim.Adam(self.D.parameters(), lr=opt.lr, betas=(0,0.99), eps=1e-8, weight_decay=0.)
+                # opt_G_state_dict['param_groups'] = self.opt_G.state_dict()['param_groups']
+                # opt_D_state_dict['param_groups'] = self.opt_D.state_dict()['param_groups']
+                # self.opt_G.load_state_dict(opt_G_state_dict)
+                # self.opt_D.load_state_dict(opt_D_state_dict)
+        print("\ndone\n")
         return current_alpha
     def update_moving_average(self, decay=0.999):
         """
@@ -213,23 +238,21 @@ class trainer:
         global_epoch = 0
         disp_circle = 10 if opt.unit_epoch > 10 else 1
         total_stages = int(math.log2(opt.size/4)) + 1
-        fixed_z = torch.FloatTensor(opt.batch_size, opt.nz).normal_(0.0, 1.0).to('cpu')
         for stage in range(1, total_stages+1):
             current_size = self.intial_size * (2 ** (stage-1))
             if stage == 1:
                 M = opt.unit_epoch
-            elif stage == 2:
+            elif stage <= 4:
                 M = opt.unit_epoch * 2
             else:
                 M = opt.unit_epoch * 3
-            ticker = 0
             for epoch in range(M):
+                current_alpha = self.update_trainer(stage, epoch)
+                self.writer.add_scalar('archive/current_alpha', current_alpha, global_epoch)
                 torch.cuda.empty_cache()
                 for aug in range(opt.num_aug):
                     for i, data in enumerate(self.dataloader, 0):
-                        current_alpha = self.update_trainer(stage, ticker)
-                        self.writer.add_scalar('archive/current_alpha', current_alpha, global_step)
-                        real_data_current, __ = data
+                        real_data_current = data
                         real_data_current = F.adaptive_avg_pool2d(real_data_current, current_size)
                         if stage > 1:
                             real_data_previous = F.interpolate(F.avg_pool2d(real_data_current, 2), scale_factor=2., mode='nearest')
@@ -247,17 +270,16 @@ class trainer:
                             print("[stage {}/{}][epoch {}/{}][aug {}/{}][iter {}/{}] G_loss {:.4f} D_loss {:.4f} W_Dist {:.4f}" \
                                 .format(stage, total_stages, epoch+1, M, aug+1, opt.num_aug, i+1, len(self.dataloader), G_loss, D_loss, Wasserstein_Dist))
                         global_step += 1
-                        ticker += 1
                 global_epoch += 1
                 if epoch % disp_circle == disp_circle-1:
                     print('\nlog images...\n')
-                    I_real = utils.make_grid(real_data, nrow=8, normalize=True, scale_each=True)
+                    I_real = utils.make_grid(real_data, nrow=4, normalize=True, scale_each=True)
                     self.writer.add_image('stage_{}/real'.format(stage), I_real, epoch)
                     with torch.no_grad():
                         self.G_EMA.eval()
-                        # z = torch.FloatTensor(real_data.size(0), opt.nz).normal_(0.0, 1.0).to('cpu')
-                        fake_data = self.G_EMA.forward(fixed_z)
-                        I_fake = utils.make_grid(fake_data, nrow=8, normalize=True, scale_each=True)
+                        z = torch.FloatTensor(real_data.size(0), opt.nz).normal_(0.0, 1.0).to('cpu')
+                        fake_data = self.G_EMA.forward(z)
+                        I_fake = utils.make_grid(fake_data, nrow=4, normalize=True, scale_each=True)
                         self.writer.add_image('stage_{}/fake'.format(stage), I_fake, epoch)
             # after each stage: save checkpoints
             print('\nsaving checkpoints...\n')
